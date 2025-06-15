c92b45884156c619e6474173638d1da421c30d02
asyaisbil <asyaisbill@gmail.com>
2025-06-15 14:55:57 +0300

    order page

---
+++ src/components/Order/Header.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <img
+                src="/../../images/iteration-1-images/logo.svg"
+            />
+            <div className="order-buttons">
+                <button>Anasayfa</button>
+                <p>-</p>
+                <button>Seçenekler</button>
+                <p>-</p>
+                <button>Sipariş Oluştur</button>
+            </div>
+        </header>
+    )
+}
